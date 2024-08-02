fd2ace87e3e3d4e7f63a6fda68b759a932295eb0
jackson <innofree2145@gmail.com>
2024-08-02 17:30:52 +0900

    test(Logo): 로고 애니메이션

--- src/ui/layout/Logo.tsx
+++ src/ui/layout/Logo.tsx
@@ -1,6 +1,35 @@
-function Logo() {
-  const classes = ["w-20 justify-center items-center flex bg-blue"].join(" ");
-  return <div className={classes}>innofree</div>;
-}
+import styled from "styled-components";
 
+function Logo() {
+  return (
+    <LogoStyle>
+      <LogoItem>innofree</LogoItem>
+    </LogoStyle>
+  );
+}
+const LogoStyle = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 200px;
+  height: 100px; /* 로고 높이에 맞게 조정 */
+  background-color: #f1f1f1; /* 배경색 선택 */
+`;
+
+const LogoItem = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+  width: 100%;
+  animation: move-left 4s ease-in-out infinite;
+
+  @keyframes move-left {
+    0% {
+      left: 100%;
+    }
+    100% {
+      left: -100%;
+    }
+  }
+`;
 export default Logo;
